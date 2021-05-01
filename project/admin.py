44e2f862d66796a74cf671a312fb5b98e2b7b669
costar011 <dnaer456@gmail.com>
2021-05-01 15:10:21 +0900

    add user

--- project/admin.py
+++ project/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from . import models
+
+
+@admin.register(models.ProjectModel)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "displayUser",
+    )
+    raw_id_fields = ("author",)
