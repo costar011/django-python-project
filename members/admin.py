fe219dfa42507158f035ecea37648d5d5b155f0a
costar011 <dnaer456@gmail.com>
2021-05-04 22:06:45 +0900

    install pillow

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.SkillModel)
+class SkillModel(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.MemberModel)
+class MemberAdmin(admin.ModelAdmin):
+    """Member Admin Definition"""
+
+    list_display = (
+        "name",
+        "gender",
+        "view_mobile",
+        "email",
+        "count_skills",
+    )
+
+    list_filter = ("gender",)
+    filter_horizontal = ("skills",)
--- members/models.py
+++ members/models.py
@@ -1,3 +1,61 @@
+from os import name
 from django.db import models
+from django.db.models.base import Model
 
-# Create your models here.
+
+class SkillModel(models.Model):
+    """Skill Model Definition"""
+
+    skill_name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.skill_name
+
+    class Meta:
+        verbose_name_plural = "SKILLS"
+
+
+class MemberModel(models.Model):
+    """Member Model Definition"""
+
+    GENDER_MALE = "Male"
+    GENDER_FEMALE = "Female"
+    GENDER_OTHER = "Other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    name = models.CharField(max_length=30)
+    birth = models.DateField()
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10)
+    avatar = models.ImageField()
+    mobile = models.CharField(max_length=12)
+    email = models.EmailField(max_length=50)
+    description = models.TextField(blank=True)
+    skills = models.ManyToManyField("SkillModel", blank=True)
+
+    # self :  나 자신
+    def __str__(self):
+        return self.name
+
+    def view_mobile(self):
+        current_value = self.mobile
+        max_len = len(current_value)
+        first_mobile = current_value[0:3]
+        second_mobile = current_value[3:7]
+        third_mobile = current_value[7:max_len]
+
+        return f"{first_mobile}-{second_mobile}-{third_mobile}"
+
+    view_mobile.short_desciprtion = "MOBILE"
+
+    def count_skills(self):
+        return f"{( self.skills.all().count())}개"
+
+    count_skills.short_description = "SKILLS"
+
+    class Meta:
+        verbose_name_plural = "MEMBERS"
