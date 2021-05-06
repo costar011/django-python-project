b3096191c2d2a6579e70b7fb1d122acd2b10e18d
costar011 <dnaer456@gmail.com>
2021-05-06 22:05:42 +0900

    members model

--- members/admin.py
+++ members/admin.py
@@ -5,19 +5,3 @@ from . import models
 @admin.register(models.SkillModel)
 class SkillModel(admin.ModelAdmin):
     pass
-
-
-@admin.register(models.MemberModel)
-class MemberAdmin(admin.ModelAdmin):
-    """Member Admin Definition"""
-
-    list_display = (
-        "name",
-        "gender",
-        "view_mobile",
-        "email",
-        "count_skills",
-    )
-
-    list_filter = ("gender",)
-    filter_horizontal = ("skills",)
