998a567346a94ff39dcacdfaf54518943f0dcf9e
costar011 <dnaer456@gmail.com>
2021-05-13 21:23:37 +0900

    models&admin

--- members/admin.py
+++ members/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.SkillModel)
+class SkillModel(admin.ModelAdmin):
+    pass
--- members/models.py
+++ members/models.py
@@ -1,3 +1,15 @@
+from os import name
 from django.db import models
+from django.db.models.base import Model
 
-# Create your models here.
+
+class SkillModel(models.Model):
+    """Skill Model Definition"""
+
+    skill_name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.skill_name
+
+    class Meta:
+        verbose_name_plural = "SKILLS"
